c46bae2ac858cb27411c83e17b15fc18d7683468
Bogdan Iordache <iordache_bogdan1998@yahoo.com>
2020-03-23 21:49:58 +0200

    Testing functionality

--- model/imagesearch/model.py
+++ model/imagesearch/model.py
@@ -5,32 +5,32 @@ class Autoencoder(nn.Module):
     def __init__(self):
         super(Autoencoder, self).__init__()
         self.encoder = nn.Sequential(
-            nn.Conv2d(3, 16, 3, stride=2, padding="same"),
+            nn.Conv2d(3, 16, 3, stride=2, padding=1),
             nn.ReLU(True),
-            nn.Conv2d(16, 32, 3, stride=2, padding="same"),
+            nn.Conv2d(16, 32, 3, stride=2, padding=1),
             nn.ReLU(True),
-            nn.Conv2d(32, 64, 3, stride=2, padding="same"),
+            nn.Conv2d(32, 64, 3, stride=2, padding=1),
             nn.ReLU(True),
-            nn.Conv2d(64, 128, 3, stride=2, padding="same"),
+            nn.Conv2d(64, 128, 3, stride=2, padding=1),
             nn.ReLU(True),
-            nn.Conv2d(128, 256, 3, stride=2, padding="same"),
+            nn.Conv2d(128, 256, 3, stride=2, padding=1),
             nn.ReLU(True),
-            nn.Conv2d(256, 512, 3, stride=2, padding="same"),
+            nn.Conv2d(256, 512, 3, stride=2, padding=1),
             nn.ReLU(True)
         )
         self.decoder = nn.Sequential(
-            nn.Conv2d(512, 512, 3, stride=2, padding="same"),
+            nn.ConvTranspose2d(512, 512, 3, stride=2, padding=1, output_padding=1),
             nn.ReLU(True),
-            nn.Conv2d(512, 256, 3, stride=2, padding="same"),
+            nn.ConvTranspose2d(512, 256, 3, stride=2, padding=1, output_padding=1),
             nn.ReLU(True),
-            nn.Conv2d(256, 128, 3, stride=2, padding="same"),
+            nn.ConvTranspose2d(256, 128, 3, stride=2, padding=1, output_padding=1),
             nn.ReLU(True),
-            nn.Conv2d(128, 64, 3, stride=2, padding="same"),
+            nn.ConvTranspose2d(128, 64, 3, stride=2, padding=1, output_padding=1),
             nn.ReLU(True),
-            nn.Conv2d(64, 32, 3, stride=2, padding="same"),
+            nn.ConvTranspose2d(64, 32, 3, stride=2, padding=1, output_padding=1),
             nn.ReLU(True),
-            nn.Conv2d(32, 3, 3, stride=2, padding="same"),
-            nn.ReLU(True)
+            nn.ConvTranspose2d(32, 3, 3, stride=2, padding=1, output_padding=1),
+            nn.Tanh()
         )
 
     def forward(self, x):
--- model/imagesearch/util.py
+++ model/imagesearch/util.py
@@ -21,24 +21,6 @@ class AverageMeter(object):
         self.avg = self.sum / self.cnt
 
 
-def accuracy(y_pred, y_actual, topk=(1, )):
-    """Computes precision@k"""
-
-    maxk = max(topk)
-    batch_size = y_actual.size(0)
-
-    _, pred = y_pred.topk(maxk, 1, True, True)
-    pred = pred.t()
-    correct = pred.eq(y_actual.view(1, -1).expand_as(pred))
-
-    res = []
-    for k in topk:
-        correct_k = correct[:k].view(-1).float().sum(0)
-        res.append(correct_k.mul_(100.0 / batch_size))
-
-    return res
-
-
 def save_checkpoint(state, is_best, filename='checkpoint.pth'):
     torch.save(state, filename)
     if is_best:
