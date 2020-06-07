1c992af1c1da9b9ebf5b627bf9dda0b4f89a1bf6
bciobanu <11034715+bciobanu@users.noreply.github.com>
2020-06-07 13:33:51 +0300

    Load python predictor modules

--- dashboard/lib/dashboard/predictor.ex
+++ dashboard/lib/dashboard/predictor.ex
@@ -1,18 +1,21 @@
 defmodule Dashboard.Predictor do
   use GenServer
 
+  # computed at compile time, so cwd will be `pain/dashboard` expanded as an absolute path
+  @model_path (File.cwd!() <> "/../model") |> Path.expand() |> to_charlist
+
   def start_link(state) do
     GenServer.start_link(__MODULE__, state, [])
   end
 
   def init(_state) do
-    {:ok, pid} = :python.start()
+    {:ok, pid} = :python.start(python_path: @model_path)
     state = %{pid: pid}
     {:ok, state}
   end
 
   def handle_call(:test, _from, state) do
-    {:reply, :python.call(state.pid, :operator, :add, [1, 1]), state}
+    {:reply, :python.call(state.pid, :predictor, :main, []), state}
   end
 
   def terminate(_reason, state) do
